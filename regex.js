60f42ede25ab4b60fd701c5ea6f8aab2922b79a5
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-26 17:14:25 -0500

    initial commit

---
+++ regex.js
@@ -0,0 +1,11 @@
+// Regex Notes
+
+// In JS, regex will be enclosed in "/ /"
+
+// .test() method takes the regex and applies it to a string 
+// returns true or false
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+console.log(result);
